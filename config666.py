e61f6f16385091c9df3607e2b93885f716657abc
ArturS5 <151950794+ArturS5@users.noreply.github.com>
2024-03-28 21:23:16 +0300

    Create config666.py

---
+++ config666.py
@@ -0,0 +1,20 @@
+#Секретные константы
+TOKEN=''
+iam_token=''
+folder_id=''
+
+#bot666.py
+MAX_SESSIONS=3
+CONTINUE_STORY='Продолжи сюжет в 1-3 предложения и оставь интригу. Не пиши никакой пояснительный текст от себя.'
+END_STORY='Напиши завершение истории с неожиданной развязкой. Не пиши никакой пояснительный текст от себя.'
+
+#gpt666.py
+SYSTEM_PROMPT=('Ты пишешь историю вместе с человеком. Пишите историю по очереди.')
+URL='https://llm.api.cloud.yandex.net/foundationModels/v1/completion'
+TEMPERATURE=0.6
+MAX_TOKENS =30
+
+#limitation666.py
+MAX_USERS=2
+MAX_TOKENS_IN_SESSION=50
+MAX_MODEL_TOKENS=200
